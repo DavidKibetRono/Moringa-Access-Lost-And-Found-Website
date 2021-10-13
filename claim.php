2acfbe00a514724164f001f0ae4b1e9fda24e7ba
DavidKibetRono <david.rono@student.moringaschool.com>
2021-10-13 15:17:35 +0300

    removed input labels

--- claim.php
+++ claim.php
@@ -12,23 +12,17 @@
 <body>
 <form action="" method= "POST" enctype="multipart/form-data" autocomplete="off" >
 <h1>Client's Claim Form</h1>
-<label>Client Name</label><br>
 <input type="text"  name="name" placeholder="Your  name" required><br>
 
 
-<label>Email Address</label><br>
 <input type="email"   name="email" placeholder = "Your email address" required><br>
 
-<label>Telephone Number</label><br>
 <input type="tel"  name="mynumber" placeholder="Telephone" required><br>
 
-<label>Item's Name</label><br>
 <input type="text"  name="name" placeholder="Your  name" required><br>
 
-<label>Item's Description</label><br>
 <textarea type="" name="description" placeholder="Describe your item" required></textarea><br>
 
-<label>Location</label><br>
 <input type="text"  name="location" placeholder="Location" required><br>
 
 <input type="file" name="image"><br>
